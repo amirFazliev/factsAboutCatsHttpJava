593d8bef1954c517129c7258323cf18eca25bea6
Amir Fazliev <fazliev.amir@yandex.ru>
2022-11-27 19:45:22 +0300

    Homework commit

---
+++ src/main/java/org/example/JsonObject.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Comparator;
+
+public class JsonObject {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvotes;
+
+    public JsonObject(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") Integer upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post" +
+                "\n id=" + id +
+                "\n text=" + text +
+                "\n type=" + type +
+                "\n user=" + user +
+                "\n upvotes=" + upvotes;
+    }
+
+
+}
